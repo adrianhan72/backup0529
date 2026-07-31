ee13091402d5f12f0733b27378b4220a9bba64d0
adrianhan72 <adrianhan72@gmail.com>
2026-07-31 10:20:11 +0900

    fix: CORS 사설 IP 대역 허용 (192.168.x.x, 10.x.x.x, 172.16-31.x.x) - 외부 접속 로그인 오류 해결

--- middleware/cors.js
+++ middleware/cors.js
@@ -9,11 +9,14 @@ module.exports = function() {
   ];
   return cors({
     origin: (origin, callback) => {
-      if (!origin || ALLOWED_ORIGINS.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
+      // localhost, 127.0.0.1, 또는 192.168.x.x 등 사설 IP 모두 허용
+      if (!origin) return callback(null, true);
+      if (ALLOWED_ORIGINS.includes(origin)) return callback(null, true);
+      // 사설 IP 대역 (192.168.x.x, 10.x.x.x, 172.16-31.x.x) 및 localhost 허용
+      if (/^https?:\/\/(localhost|127\.0\.0\.1|192\.168\.\d+\.\d+|10\.\d+\.\d+\.\d+|172\.(1[6-9]|2\d|3[01])\.\d+\.\d+)(:\d+)?$/.test(origin)) {
+        return callback(null, true);
       }
+      callback(new Error('Not allowed by CORS'));
     },
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
